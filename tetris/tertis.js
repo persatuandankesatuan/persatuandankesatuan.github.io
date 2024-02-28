010e0348a9beba2e2e02b96a19b4cd2deadb1e5e
persatuandankesatuan <160716912+persatuandankesatuan@users.noreply.github.com>
2024-02-28 15:07:58 +0700

    Create tertis.js

---
+++ tetris/tertis.js
@@ -0,0 +1,12 @@
+// tetris.js
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.game-board');
+  let squares = Array.from(grid.querySelectorAll('div'));
+  const scoreDisplay = document.getElementById('score');
+  const startBtn = document.querySelector('.start-button');
+  const width = 10;
+  let currentIndex = 0;
+  let timerId;
+
+  // Code goes here
+});
